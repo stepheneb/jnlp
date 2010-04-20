535e05974ead80a8cba681b16c61254948c2df80
Stephen Bannasch <stephen.bannasch@gmail.com>
2010-04-20 01:06:47 -0400

    this require should not be needed

--- lib/jnlp/otrunk.rb
+++ lib/jnlp/otrunk.rb
@@ -79,8 +79,6 @@ end
 module Jnlp #:nodoc:
   # 
   #
-  require "#{File.expand_path(File.dirname(__FILE__))}/jnlp.rb"
-  #
   # Jnlp::Otrunk is a subclass of Jnlp::Jnlp that adds SAIL-Otrunk[https://confluence.concord.org/display/CSP/OTrunk]
   # specific methods for execution.of the jnlp locally without 
   # using Java Web Start.
